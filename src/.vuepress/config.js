0008f55fc45be55c081f896c814e5c2ed8fbacf4
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-10-30 13:18:37 +0800

    Add Tailwind

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,6 +1,11 @@
 module.exports = {
     title: 'Iridium',
     description: 'A UI framework with no UI',
-    base: '/',
+    postcss: {
+        plugins: [
+            require("tailwindcss")("./tailwind.js"),
+            require("autoprefixer")
+        ]
+    },
     themeConfig: {}
 }
