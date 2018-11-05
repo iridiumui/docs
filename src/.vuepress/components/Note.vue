391481bf3b907c6ea72e85d44aeda11b32aa8bd8
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-11-05 14:56:11 +0800

    Create note component

---
+++ src/.vuepress/components/Note.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="note bg-blue p-4 mb-4 text-base text-white">
+        <span class="font-bold block mb-2">{{ heading }}</span>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            heading: {
+                type: String,
+                default: 'Note'
+            }
+        }
+    }
+</script>
