7783c0967818d3756781a4d483139e08679d9c14
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-11-05 14:35:16 +0800

    Remove old code from layout

--- src/.vuepress/theme/Layout.vue
+++ src/.vuepress/theme/Layout.vue
@@ -27,16 +27,6 @@
                 </ir-transition-fade-slide-up>
             </ir-scroll-to>
         </ir-in-view>
-        <!--
-        <ScrollTop>
-            <button class="fixed" style="bottom: 2rem; right: 2rem;" type="button" slot-scope="{ scroll, show }" v-show="show" @click="scroll">
-                <ir-visually-hidden>
-                    <span>Scroll to top</span>
-                </ir-visually-hidden>
-                <svg class="w-8 h-8 text-grey-light" fill="currentColor" viewBox="0 0 27 15" xmlns="http://www.w3.org/2000/svg"><path d="M14.837.54l11.609 11.314a1.81 1.81 0 0 1 0 2.606c-.738.72-1.936.72-2.674 0L13.5 4.45 3.228 14.46c-.739.72-1.936.72-2.674 0a1.81 1.81 0 0 1 0-2.606L12.164.539C12.532.18 13.015 0 13.5 0c.484 0 .968.18 1.337.54z"/></svg>
-            </button>
-        </ScrollTop>
-        -->
     </div>
 </template>
 
