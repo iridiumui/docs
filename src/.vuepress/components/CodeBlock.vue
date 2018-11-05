e35ac29a80e17273845664cee83273a23888996b
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-11-05 11:13:47 +0800

    Update CodeBlock to use Iridium

--- src/.vuepress/components/CodeBlock.vue
+++ src/.vuepress/components/CodeBlock.vue
@@ -1,77 +1,18 @@
 <template>
-    <div class="relative">
-        <slot></slot>
+    <ir-code-block class="relative">
+        <div slot="content">
+            <slot></slot>
+        </div>
         <button
             type="button"
+            slot="button"
+            slot-scope="{ copy, copied }"
             @click="copy"
             class="absolute pin-t pin-r p-2 text-grey font-semibold"
             :class="{ 'text-grey-light': copied }"
             :disabled="copied"
         >
-            {{ text }}
+            <span v-text="copied ? 'Copied' : 'Copy'"></span>
         </button>
-    </div>
+    </ir-code-block>
 </template>
-
-<script>
-    export default {
-        props: {
-            successCallback: {
-                type: Function,
-                required: false
-            },
-
-            errorCallback: {
-                type: Function,
-                required: false
-            },
-        },
-
-        data() {
-            return {
-                text: 'Copy',
-                copied: false
-            }
-        },
-
-        methods: {
-            copy() {
-                const selection = window.getSelection()
-                const range = document.createRange()
-
-                range.selectNodeContents(this.$el.querySelector('pre code'))
-                selection.removeAllRanges()
-                selection.addRange(range)
-
-                try {
-                    document.execCommand('copy')
-                    selection.removeAllRanges()
-
-                    if (this.successCallback) {
-                        this.successCallback()
-                    } else {
-                        this.text = 'Copied'
-                        this.copied = true
-
-                        setTimeout(() => {
-                            this.text = 'Copy'
-                            this.copied = false
-                        }, 2000)
-                    }
-                } catch (error) {
-                    if (this.errorCallback) {
-                        this.errorCallback()
-                    } else {
-                        this.text = 'Error'
-                        this.copied = true
-
-                        setTimeout(() => {
-                            this.text = 'Error'
-                            this.copied = false
-                        }, 2000)
-                    }
-                }
-            }
-        }
-    }
-</script>
