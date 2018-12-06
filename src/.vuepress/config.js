0c0655e07d1bc48b5456de652fdde87c72105709
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-12-06 12:08:25 +0700

    Split CSS into smaller files

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -17,6 +17,7 @@ module.exports = {
     ],
     postcss: {
         plugins: [
+            require("postcss-import"),
             require("tailwindcss")("./tailwind.js"),
             require("autoprefixer")
         ]
