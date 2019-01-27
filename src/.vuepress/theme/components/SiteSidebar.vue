88fd7fca76d8a26a2a1f73f7f100c15d7afe1a49
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2019-01-27 19:42:38 +0100

    Update sitebar to render on server

--- src/.vuepress/theme/components/SiteSidebar.vue
+++ src/.vuepress/theme/components/SiteSidebar.vue
@@ -1,25 +1,23 @@
 <template>
-    <ir-screen-lg>
-        <aside class="w-64 pt-16 h-screen xl:w-80 sticky pin-t">
-            <nav class="bg-grey-lightest border-t border-grey-lighter overflow-y-auto h-full pt-8 md:pt-12">
-                <nav-list>
-                    <ir-screen-lg slot="footer" slot-scope="{ globalNavItems }" direction="down">
-                        <ul class="list-reset px-12 py-8 border-t border-grey-lighter">
-                            <li v-for="item in globalNavItems">
-                                <a
-                                    :href="item.url"
-                                    target="_blank"
-                                    class="sidebar-link text-grey-dark hover:text-grey-darker"
-                                >
-                                    {{ item.name }}
-                                </a>
-                            </li>
-                        </ul>
-                    </ir-screen-lg>
-                </nav-list>
-            </nav>
-        </aside>
-    </ir-screen-lg>
+    <aside class="hidden lg:block w-64 pt-16 h-screen xl:w-80 sticky pin-t">
+        <nav class="bg-grey-lightest border-t border-grey-lighter overflow-y-auto h-full pt-8 md:pt-12">
+            <nav-list>
+                <ir-screen-lg slot="footer" slot-scope="{ globalNavItems }" direction="down">
+                    <ul class="list-reset px-12 py-8 border-t border-grey-lighter">
+                        <li v-for="item in globalNavItems">
+                            <a
+                                :href="item.url"
+                                target="_blank"
+                                class="sidebar-link text-grey-dark hover:text-grey-darker"
+                            >
+                                {{ item.name }}
+                            </a>
+                        </li>
+                    </ul>
+                </ir-screen-lg>
+            </nav-list>
+        </nav>
+    </aside>
 </template>
 
 <script>
